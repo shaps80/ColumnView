a9a6e5e4b20db2cb2e741f26b93e5e3679441886
Shaps Benkau <shapsuk@me.com>
2019-07-01 14:25:54 +0100

    Initial Commit

---
+++ ColumnsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColumnsDemo
+//
+//  Created by Shaps Benkau on 01/07/2019.
+//  Copyright © 2019 152 Percent Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
